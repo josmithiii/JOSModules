8980909de488417edd01aff40908a9400901477f
Julius Smith <2200853+josmithiii@users.noreply.github.com>
2024-10-04 10:17:22 -0700

    main: Doxyfile changes - snapshot of decent looking doc at ccrma ( dated today here )

---
+++ docs/doxygen/jos_modules_ccrma_2024-10-04/search/searchdata.js
@@ -0,0 +1,48 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefghijklmnoprstuvwxz~",
+  1: "abcdfgjlmprstxz",
+  2: "cdir",
+  3: "cfj",
+  4: "abcdefghijklmnprstuxz~",
+  5: "abcdefilmnoprst",
+  6: "s",
+  7: "clpst",
+  8: "bcdfglnoprstx",
+  9: "f",
+  10: "efghjlosvw",
+  11: "jt"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables",
+  6: "typedefs",
+  7: "enums",
+  8: "enumvalues",
+  9: "defines",
+  10: "groups",
+  11: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Variables",
+  6: "Typedefs",
+  7: "Enumerations",
+  8: "Enumerator",
+  9: "Macros",
+  10: "Modules",
+  11: "Pages"
+};
+
